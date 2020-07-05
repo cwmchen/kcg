44e40a117757ddcb432b87edee5db553e293019a
weiming_chen <cwm_chen@126.com>
2020-07-05 20:16:40 +0800

    接口跨域访问配置

--- kcg-dashboard/src/main/java/com/kaadog/kcg/dashboard/config/DashboardConfiguration.java
+++ kcg-dashboard/src/main/java/com/kaadog/kcg/dashboard/config/DashboardConfiguration.java
@@ -15,9 +15,14 @@
  */
 package com.kaadog.kcg.dashboard.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -35,11 +40,116 @@ public class DashboardConfiguration {
     private String             fileName                = DEFAULT_FILE_NAME;
     /** 接口服务地址 */
     private String             apiServerPath           = DEFAULT_API_SERVER_PATH;
+    
+    /** 跨域注册器 */
+    private List<CorsRegistration>       corsRegistrations;
 
     public String getFileName() {
         if (StringUtils.isBlank(fileName)) {
             return DEFAULT_FILE_NAME;
         }
         return fileName;
+    }
+
+    public static class CorsRegistration {
+
+        public static final String            ALL                            = "*";
+
+        private static final List<HttpMethod> DEFAULT_METHODS                = Arrays
+                .asList(HttpMethod.GET, HttpMethod.HEAD, HttpMethod.POST, HttpMethod.PUT, HttpMethod.PATCH,
+                        HttpMethod.DELETE, HttpMethod.OPTIONS, HttpMethod.TRACE);
+
+        private static final List<String>     DEFAULT_PERMIT_METHODS         = Arrays
+                .asList(HttpMethod.GET.name(), HttpMethod.HEAD.name(), HttpMethod.POST.name());
+
+        private static final List<String>     DEFAULT_PERMIT_ALL             = Arrays.asList(ALL);
+
+        private static final List<String>     DEFAULT_PERMIT_EXPOSED_HEADERS = Arrays
+                .asList(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN);
+
+        /** 请求地址, 支持 SpEL */
+        private String                        pathPattern                    = "/**";
+        /** 哪些域名可以被允许 */
+        private List<String>                  allowedOrigins                 = DEFAULT_PERMIT_ALL;
+        /** 请求的哪些方法可以被允许 */
+        private List<String>                  allowedMethods                 = DEFAULT_PERMIT_METHODS;
+        /** 可以参与跨域访问的请求方法, 一般比 allowedMethods 多 */
+        private List<HttpMethod>              resolvedMethods                = DEFAULT_METHODS;
+        /** 表示跨域请求的头部的允许范围 */
+        private List<String>                  allowedHeaders                 = DEFAULT_PERMIT_ALL;
+        /** 表示暴露哪些头部信息，并提供给客户端 */
+        private List<String>                  exposedHeaders                 = DEFAULT_PERMIT_EXPOSED_HEADERS;
+        /** 表示是否允许客户端获取用户凭据 */
+        private Boolean                       allowCredentials               = true;
+        /** 表示预检请求的最大缓存时间 */
+        private Long                          maxAge                         = 1800L;
+
+        public String getPathPattern() {
+            return pathPattern;
+        }
+
+        public void setPathPattern(String pathPattern) {
+            this.pathPattern = pathPattern;
+        }
+
+        public String[] getAllowedOrigins() {
+            return allowedOrigins.stream().toArray(String[]::new);
+        }
+
+        public void setAllowedOrigins(List<String> allowedOrigins) {
+            this.allowedOrigins = allowedOrigins;
+        }
+
+        public String[] getAllowedMethods() {
+            return allowedMethods.stream().toArray(String[]::new);
+        }
+
+        public void setAllowedMethods(List<String> allowedMethods) {
+            this.allowedMethods = allowedMethods;
+        }
+
+        public HttpMethod[] getResolvedMethods() {
+            return resolvedMethods.stream().toArray(HttpMethod[]::new);
+        }
+
+        public void setResolvedMethods(List<HttpMethod> resolvedMethods) {
+            this.resolvedMethods = resolvedMethods;
+        }
+
+        public String[] getAllowedHeaders() {
+            return allowedHeaders.stream().toArray(String[]::new);
+        }
+
+        public void setAllowedHeaders(List<String> allowedHeaders) {
+            this.allowedHeaders = allowedHeaders;
+        }
+
+        public String[] getExposedHeaders() {
+            if (exposedHeaders == null) {
+                return null;
+            }
+
+            return exposedHeaders.stream().toArray(String[]::new);
+        }
+
+        public void setExposedHeaders(List<String> exposedHeaders) {
+            this.exposedHeaders = exposedHeaders;
+        }
+
+        public Boolean getAllowCredentials() {
+            return allowCredentials;
+        }
+
+        public void setAllowCredentials(Boolean allowCredentials) {
+            this.allowCredentials = allowCredentials;
+        }
+
+        public Long getMaxAge() {
+            return maxAge;
+        }
+
+        public void setMaxAge(Long maxAge) {
+            this.maxAge = maxAge;
+        }
     }
 }
